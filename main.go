6486218765173d8126f627cd3f89a58fce7e2109
wander <wander@ffactory.org>
2023-02-23 20:56:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:            "hiposter",
-		Width:            800,
-		Height:           600,
+		Width:            1024,
+		Height:           768,
 		WindowStartState: options.Maximised,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
